a0eba3b1fa8a5b64a92a651abbeb28d646dae5ae
Wayne Lewis <waynelewis@esss.se>
2018-12-04 22:05:14 +0100

    ENH: Add EVR-specific commands

--- st.hzb-v20-evr-05.cmd
+++ st.hzb-v20-evr-05.cmd
@@ -1,3 +1,5 @@
+require evr-timestamp-buffer,2.5.0
+
 epicsEnvSet("SYS", "HZB-V20:TS")
 epicsEnvSet("PCI_SLOT_0", "05:0e.0")
 epicsEnvSet("PCI_SLOT_1", "06:0b.0")
@@ -17,7 +19,13 @@ epicsEnvSet("NCG_SYS", "HZB-V20:")
 epicsEnvSet("NCG_DRV", "Chop-Drv-05tmp:")
 ##################################################
 
-< "$(EPICS_CMDS)/mrfioc2-common-cmd/7_st.evr.cmd"
+< "$(EPICS_CMDS)/mrfioc2-common-cmd/st.evr.cmd"
+
+# Load EVR database
+dbLoadRecords("$(MRF_HW_DB)","EVR=$(EVR),SYS=$(SYS),D=$(DEVICE),FEVT=88.0525,PINITSEQ=0")
+
+# Load timestamp buffer database
+iocshLoad("$(evr-timestamp-buffer_DIR)/evr-timestamp-buffer.iocsh", "CHIC_SYS=$(CHIC_SYS), CHIC_DEV=$(CHIC_DEV), CHOP_DRV=$(CHOP_DRV), SYS=$(SYS)")
 
 iocInit()
 
